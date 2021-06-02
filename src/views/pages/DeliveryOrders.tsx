6a29535bac3651970b8d08e5e70c8579c12b5a1e
Umang Lohia <ulohia@umich.edu>
2021-06-02 12:18:06 +0530

    Fixing conditionals

--- src/views/pages/DeliveryOrders.tsx
+++ src/views/pages/DeliveryOrders.tsx
@@ -39,6 +39,8 @@ import {
   Input,
   UncontrolledTooltip,
   FormGroup,
+  Button,
+  Col,
 } from "reactstrap";
 // core components
 import Header from "../../components/Headers/Header";
@@ -69,12 +71,17 @@ const DeliveryOrders: React.FC = () => {
     }
   }, []);
 
-  const handleClick = () => {
+  const handleFileInErp = () => {
     if (selected !== undefined) {
       const order = deliveryOrders[selected];
       let customerInfo: any, additionalInfo: any, vehicleInfo: any;
 
       if (order.customerInfo && order.vehicleInfo && order.additionalInfo) {
+        window.api.send("toMain", {
+          type: "FILE_IN_ERP",
+          data: order,
+        });
+      } else {
         db.collection("customers")
           .doc(order.customerId)
           .get()
@@ -102,6 +109,10 @@ const DeliveryOrders: React.FC = () => {
                           const tempOrders = deliveryOrders;
                           tempOrders[selected] = fullDetails;
                           setDeliveryOrders(tempOrders);
+                          window.api.send("toMain", {
+                            type: "FILE_IN_ERP",
+                            data: fullDetails,
+                          });
                         }
                       });
                   }
@@ -109,6 +120,14 @@ const DeliveryOrders: React.FC = () => {
             }
           });
       }
+    }
+  };
+
+  const toggleSelected = (index: number) => {
+    if (selected === index) {
+      setSelected(undefined);
+    } else {
+      setSelected(index);
     }
   };
 
@@ -122,7 +141,34 @@ const DeliveryOrders: React.FC = () => {
           <div className="col">
             <Card className="shadow">
               <CardHeader className="border-0">
-                <h3 className="mb-0">Delivery Order </h3>
+                <Row>
+                  <Col xs="8">
+                    <h3 className="my-3">Delivery Order</h3>
+                  </Col>
+                  {selected !== undefined && (
+                    <Col
+                      className="d-flex justify-content-end align-items-center"
+                      xs="4"
+                    >
+                      <Button
+                        className="small-button-width my-2"
+                        color={"danger"}
+                        onClick={() => {}}
+                        size="sm"
+                      >
+                        Delete
+                      </Button>
+                      <Button
+                        className="small-button-width my-2"
+                        color={"primary"}
+                        onClick={handleFileInErp}
+                        size="sm"
+                      >
+                        File in ERP
+                      </Button>
+                    </Col>
+                  )}
+                </Row>
               </CardHeader>
               {deliveryOrders.length > 0 ? (
                 <Table className="align-items-center table-flush" responsive>
@@ -145,7 +191,7 @@ const DeliveryOrders: React.FC = () => {
                             type="checkbox"
                             color="primary"
                             checked={index === selected}
-                            onChange={() => setSelected(index)}
+                            onChange={() => toggleSelected(index)}
                           />
                         </td>
                         <td>{curElem.name}</td>
