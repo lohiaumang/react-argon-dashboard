b5041933237bed52c05af0427da4e4ddb9b399de
Rahul kumar <rahul@brikolite.com>
2021-07-08 12:38:18 +0530

    Express Booking button not work

--- public/electron/puppeteer-scripts/erp.js
+++ public/electron/puppeteer-scripts/erp.js
@@ -22,137 +22,137 @@ module.exports = function erp(page, data, mainWindow) {
     await click(page, "#s_sctrl > #s_sctrl_tabScreen #ui-id-130");
 
     // fill data select
-    // await click(page,'table > tbody > tr > .siebui-bullet-item > #s_3_2_18_0_span > .siebui-anchor-readonly > a[rowid="5"]');
+     await click(page,'table > tbody > tr > .siebui-bullet-item > #s_3_2_18_0_span > .siebui-anchor-readonly > a[rowid="1"]');
 
     //fill new reord
-    await typeText(
-      page,
-      "table > tbody > tr > .siebui-form-data > .s_2_1_19_0",
-      data.customerInfo.firstName
-    );
-    await typeText(
-      page,
-      "table > tbody > tr > .siebui-form-data > .s_2_1_2_0",
-      ""
-    );
-    await typeText(
-      page,
-      "table > tbody > tr > .siebui-form-data > .s_2_1_18_0",
-      data.customerInfo.lastName
-    );
-    await typeText(
-      page,
-      "table > tbody > tr > .siebui-form-data > .s_2_1_0_0",
-      data.customerInfo.phoneNo
-    );
+    // await typeText(
+    //   page,
+    //   "table > tbody > tr > .siebui-form-data > .s_2_1_19_0",
+    //   data.customerInfo.firstName
+    // );
+    // await typeText(
+    //   page,
+    //   "table > tbody > tr > .siebui-form-data > .s_2_1_2_0",
+    //   ""
+    // );
+    // await typeText(
+    //   page,
+    //   "table > tbody > tr > .siebui-form-data > .s_2_1_18_0",
+    //   data.customerInfo.lastName
+    // );
+    // await typeText(
+    //   page,
+    //   "table > tbody > tr > .siebui-form-data > .s_2_1_0_0",
+    //   data.customerInfo.phoneNo
+    // );
 
-    await click(page, 'input[aria-label="Consent For Calling"] + span');
-    await page.waitForSelector(
-      "ul[role='combobox']:not([style*='display: none'])",
-      {
-        visible: true,
-      }
-    );
-    let callingType = await page.$$eval(
-      "ul[role='combobox']:not([style*='display: none']) > li > div",
-      (listItems) =>
-        listItems.map((item) => {
-          return {
-            name: item.textContent,
-            id: item.id,
-          };
-        })
-    );
-    const collingTypeButton = callingType.find((item) => item.name === "No");
-    await click(page, `#${collingTypeButton.id}`);
+    // await click(page, 'input[aria-label="Consent For Calling"] + span');
+    // await page.waitForSelector(
+    //   "ul[role='combobox']:not([style*='display: none'])",
+    //   {
+    //     visible: true,
+    //   }
+    // );
+    // let callingType = await page.$$eval(
+    //   "ul[role='combobox']:not([style*='display: none']) > li > div",
+    //   (listItems) =>
+    //     listItems.map((item) => {
+    //       return {
+    //         name: item.textContent,
+    //         id: item.id,
+    //       };
+    //     })
+    // );
+    // const collingTypeButton = callingType.find((item) => item.name === "No"); //todo
+    // await click(page, `#${collingTypeButton.id}`);
 
-    await click(page, 'input[aria-label="Gender"] + span');
-    await page.waitForSelector(
-      "ul[role='combobox']:not([style*='display: none'])",
-      {
-        visible: true,
-      }
-    );
-    let genderType = await page.$$eval(
-      "ul[role='combobox']:not([style*='display: none']) > li > div",
-      (listItems) =>
-        listItems.map((item) => {
-          return {
-            name: item.textContent,
-            id: item.id,
-          };
-        })
-    );
-    const genderTypeButton = genderType.find(
-      (item) => item.name === data.customerInfo.gender.slice(0, 1).toUpperCase()
-    );
-    await click(page, `#${genderTypeButton.id}`);
+    // await click(page, 'input[aria-label="Gender"] + span');
+    // await page.waitForSelector(
+    //   "ul[role='combobox']:not([style*='display: none'])",
+    //   {
+    //     visible: true,
+    //   }
+    // );
+    // let genderType = await page.$$eval(
+    //   "ul[role='combobox']:not([style*='display: none']) > li > div",
+    //   (listItems) =>
+    //     listItems.map((item) => {
+    //       return {
+    //         name: item.textContent,
+    //         id: item.id,
+    //       };
+    //     })
+    // );
+    // const genderTypeButton = genderType.find(
+    //   (item) => item.name === data.customerInfo.gender.slice(0, 1).toUpperCase()
+    // );
+    // await click(page, `#${genderTypeButton.id}`);
 
-    await click(page, "tbody > tr > td > #s_2_1_10_0_Ctrl > span");
-    await typeText(
-      page,
-      "tbody > tr > td > .mceGridField > .s_3_1_80_0",
-      data.customerInfo.swdo
-    );
+    // await click(page, "tbody > tr > td > #s_2_1_10_0_Ctrl > span");
+    // await typeText(
+    //   page,
+    //   "tbody > tr > td > .mceGridField > .s_3_1_80_0",
+    //   data.customerInfo.swdo
+    // );
 
-    await typeText(
-      page,
-      'tbody > tr > td > .mceGridField > input[aria-label="Date Of Birth"]',
-      data.customerInfo.dob
-    );
+    // await typeText(
+    //   page,
+    //   'tbody > tr > td > .mceGridField > input[aria-label="Date Of Birth"]',
+    //   data.customerInfo.dob
+    // );
 
-    await typeText(
-      page,
-      "tbody > tr > td > .mceGridField > .s_3_1_5_0",
-      data.customerInfo.currLineOne +
-        " " +
-        data.customerInfo.currLineTwo +
-        " " +
-        "PS" +
-        " " +
-        data.customerInfo.currPS
-    );
-    await typeText(
-      page,
-      "tbody > tr > td > .mceGridField > .s_3_1_6_0",
-      data.customerInfo.currDistrict + " " + data.customerInfo.currCity
-    );
+    // await typeText(
+    //   page,
+    //   "tbody > tr > td > .mceGridField > .s_3_1_5_0",
+    //   data.customerInfo.currLineOne +
+    //     " " +
+    //     data.customerInfo.currLineTwo +
+    //     " " +
+    //     "PS" +
+    //     " " +
+    //     data.customerInfo.currPS
+    // );
+    // await typeText(
+    //   page,
+    //   "tbody > tr > td > .mceGridField > .s_3_1_6_0",
+    //   data.customerInfo.currDistrict + " " + data.customerInfo.currCity
+    // );
 
-    await click(page, 'input[aria-label="State"] + span');
-    await page.waitForSelector(
-      "ul[role='combobox']:not([style*='display: none'])",
-      {
-        visible: true,
-      }
-    );
-    let state = await page.$$eval(
-      "ul[role='combobox']:not([style*='display: none']) > li > div",
-      (listItems) =>
-        listItems.map((item) => {
-          // console.log(item.textContent, item.id);
-          return {
-            name: item.textContent,
-            id: item.id,
-          };
-        })
-    );
-    const stateButton = state.find(
-      (item) =>
-        item.name === data.customerInfo.currState.slice(0, 2).toUpperCase()
-    );
-    //  console.log(stateButton.name, stateButton.id);
-    await page.waitForSelector(`#${stateButton.id}`, { visible: true });
-    await page.$eval(`#${stateButton.id}`, (el) => el.click());
-    await typeText(
-      page,
-      "tbody > tr > td > .mceGridField > .s_3_1_66_0",
-      data.customerInfo.currPostal
-    );
-    await typeText(
-      page,
-      "tbody > tr > td > .mceGridField > .siebui-ctrl-mailto",
-      data.customerInfo.email
-    );
+    // await click(page, 'input[aria-label="State"] + span');
+    // await page.waitForSelector(
+    //   "ul[role='combobox']:not([style*='display: none'])",
+    //   {
+    //     visible: true,
+    //   }
+    // );
+    // let state = await page.$$eval(
+    //   "ul[role='combobox']:not([style*='display: none']) > li > div",
+    //   (listItems) =>
+    //     listItems.map((item) => {
+    //       // console.log(item.textContent, item.id);
+    //       return {
+    //         name: item.textContent,
+    //         id: item.id,
+    //       };
+    //     })
+    // );
+    // const stateButton = state.find(
+    //   (item) =>
+    //     item.name === data.customerInfo.currState.slice(0, 2).toUpperCase()
+    // );
+    // //  console.log(stateButton.name, stateButton.id);
+    // await page.waitForSelector(`#${stateButton.id}`, { visible: true });
+    // await page.$eval(`#${stateButton.id}`, (el) => el.click());
+    // await typeText(
+    //   page,
+    //   "tbody > tr > td > .mceGridField > .s_3_1_66_0",
+    //   data.customerInfo.currPostal
+    // );
+    // await typeText(
+    //   page,
+    //   "tbody > tr > td > .mceGridField > .siebui-ctrl-mailto",
+    //   data.customerInfo.email
+    // );
 
     //code work
 
@@ -196,7 +196,7 @@ module.exports = function erp(page, data, mainWindow) {
     );
     await click(page, `#${enquiryButton.id}`);
 
-    //   //Customer Type
+  //Customer Type
     await click(page, 'input[aria-label="Customer Type"] + span');
     await page.waitForSelector(
       "ul[role='combobox']:not([style*='display: none'])",
@@ -219,7 +219,7 @@ module.exports = function erp(page, data, mainWindow) {
     );
     await click(page, `#${customerTypeButton.id}`);
 
-    //   //Enquiry Source
+    //Enquiry Source
     await click(page, 'input[aria-label="Enquiry Source"] + span');
     await page.waitForSelector(
       "ul[role='combobox']:not([style*='display: none'])",
@@ -242,7 +242,7 @@ module.exports = function erp(page, data, mainWindow) {
     );
     await click(page, `#${enquirySourceButton.id}`);
 
-    //   //Customer Category
+  //Customer Category
 
     await click(page, 'input[aria-label="Customer Category"] + span');
     await page.waitForSelector(
@@ -264,7 +264,7 @@ module.exports = function erp(page, data, mainWindow) {
     );
     await click(page, `#${customerCategoryButton.id}`);
 
-    //   //Enquiry Category
+  //Enquiry Category
 
     await click(page, 'input[aria-label="Enquiry Category"] + span');
     await page.waitForSelector(
@@ -308,16 +308,16 @@ module.exports = function erp(page, data, mainWindow) {
     );
     await click(page, `#${purchaseTypeButton.id}`);
 
-    //   // //Financier Category
+      //Financier Category
 
-    //   // if (purchaseTypeButton.name === "Finance") {
-    //   //   console.log("stape 1")
+     // if (purchaseTypeButton.name === "Finance") {
+       //   console.log("stape 1")
 
-    //   //   await click(page,'input[aria-label="Financier"]');
-    //   //   console.log("stape 2")
-    //   //   await typeText(page,'input[aria-label="Financier"]','HDFC BANK')
-    //   //   console.log("stape 3")
-    //   // }
+       //   await click(page,'input[aria-label="Financier"]');
+       //   console.log("stape 2")
+       //   await typeText(page,'input[aria-label="Financier"]','HDFC BANK')
+       //   console.log("stape 3")
+       // }
 
     //Model Category
 
@@ -344,7 +344,7 @@ module.exports = function erp(page, data, mainWindow) {
     );
     await click(page, `#${modelCategoryButton.id}`);
 
-    //   //Model Name
+    //Model Name
 
     await click(
       page,
@@ -369,7 +369,7 @@ module.exports = function erp(page, data, mainWindow) {
     );
     await click(page, `#${modelNameButton.id}`);
 
-    //   //Model Variant
+    //Model Variant
 
     await click(
       page,
@@ -394,7 +394,7 @@ module.exports = function erp(page, data, mainWindow) {
     );
     await click(page, `#${modelVariantButton.id}`);
 
-    //   //Assigned To (DSE)
+   //Assigned To (DSE)  todo
 
     await click(page, 'input[aria-label="Assigned To (DSE)"] + span');
     await click(page, 'input[aria-label="Find"] + span');
@@ -423,6 +423,20 @@ module.exports = function erp(page, data, mainWindow) {
       page,
       '.siebui-popup-btm > .siebui-popup-button > button[data-display="OK"]'
     );
+
+    
+
+    await page.waitForSelector(".siebui-applet-header #s_at_m_1",{ visible: true });
+    await page.$eval(".siebui-applet-header #s_at_m_1:not([style*='display: none'])", el => el.click());
+    await click(page,'#s_at_m_1-menu > li[data-caption="Save Record                [Ctrl+S]"] > .ui-menu-item-wrapper'); //todo
+    await click(page,'#s_1_l > tbody > tr > td > .drilldown');
+    await click(page,'#s_vctrl_div_tabScreen > ul[role="tablist"] li[aria-labelledby="ui-id-543"] > a[data-tabindex="tabScreen3"]');
+   // await page.waitForSelector('#s_vctrl_div_tabScreen > ul[role="tablist"] li[aria-labelledby="ui-id-543"] > a[data-tabindex="tabScreen3"]');
+   // await page.$eval('#s_vctrl_div_tabScreen > ul[role="tablist"] li[aria-labelledby="ui-id-543"] > a[data-tabindex="tabScreen3"]', el => el.click());
+   await click(page,'.siebui-applet-buttons > .siebui-btn-grp-search > button[name="s_3_1_3_0"]');
+   await click(page,'#s_3_l > tbody > .jqgrow > td[style="text-align:left;"] > .drilldown');
+    console.log("step 1")
+    
   }
 
   automate();
