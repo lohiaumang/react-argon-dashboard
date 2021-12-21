00be3faaf518576336c97f6b8dd56e50432b9ffe
Umang Lohia <ulohia@umich.edu>
2021-12-21 15:51:29 +0530

    Trying to fix icons

--- public/electron/electron.js
+++ public/electron/electron.js
@@ -30,6 +30,7 @@ function createWindow() {
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
+    icon: path.join(__dirname, "../favicon.ico"),
   });
 
   // and load the index.html of the app.
