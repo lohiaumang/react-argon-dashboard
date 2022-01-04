68b41b118c166fc2768804da790203506a4762f1
Rahul kumar <rahul@brikolite.com>
2022-01-04 10:48:20 +0530

    fix database backup in google storage

--- functions/index.js
+++ functions/index.js
@@ -227,6 +227,39 @@ exports.userDeletedIndia = functions
     return doc.delete();
   });
 
+
+const firestore = require('@google-cloud/firestore');
+const client = new firestore.v1.FirestoreAdminClient();
+
+// Replace BUCKET_NAME
+const bucket = 'gs://developmentdbtest';
+
+exports.scheduledFirestoreExport = functions.pubsub
+                                            .schedule('every 2 minutes')
+                                            .onRun((context) => {
+
+  const projectId = process.env.GCP_PROJECT || process.env.GCLOUD_PROJECT;
+  const databaseName = 
+    client.databasePath(projectId, '(default)');
+
+  return client.exportDocuments({
+    name: databaseName,
+    outputUriPrefix: bucket,
+    // Leave collectionIds empty to export all collections
+    // or set to a list of collection IDs to export,
+    // collectionIds: ['users', 'posts']
+    collectionIds: ['byWeek','bySalesMan','byMonth','byModel']
+    })
+  .then(responses => {
+    const response = responses[0];
+    console.log(`Operation Name: ${response['name']}`);
+  })
+  .catch(err => {
+    console.error(err);
+    throw new Error('Export operation failed');
+  });
+});
+
 // //Reset Pasword
 // exports.resetPasswordIndia = functions
 //   .region("asia-south1")
